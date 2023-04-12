ca108731ac24068158fd22afb255fb013f220f68
ZachiNachshon <zachi.nachshon@gmail.com>
2023-04-12 22:07:47 +0300

    [Provisioner] Fixed version not being read properly when from pip installation
    Updated lock files

--- python_core_lib/python_core_lib/cli/entrypoint.py
+++ python_core_lib/python_core_lib/cli/entrypoint.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 from typing import Callable, Optional
+from python_core_lib.infra.context import Context
 
 import typer
 
 from python_core_lib.cli.state import CliGlobalArgs
 from python_core_lib.infra.log import LoggerManager
+from python_core_lib.utils.paths import Paths
 
-STATIC_VERSION_FILE_PATH = None
+STATIC_VERSION_PACKAGE_PATH = None
 
 MODIFIERS_FLAGS_HELP_TITLE = "Modifiers"
 
@@ -74,7 +76,8 @@ def main_runner(
 def try_read_version() -> str:
     content = "no version"
     try:
-        with open(STATIC_VERSION_FILE_PATH, "r+") as opened_file:
+        file_path = Paths.create(Context.create()).get_file_path_from_python_package(STATIC_VERSION_PACKAGE_PATH, "version.txt")
+        with open(file_path, "r+") as opened_file:
             content = opened_file.read()
             opened_file.close()
     except Exception as error:
@@ -87,11 +90,11 @@ class EntryPoint:
     def create_typer(
         title: str,
         config_resolver_fn: Optional[Callable] = None,
-        version_file_path: Optional[str] = "resources/version.txt",
+        version_package_path: Optional[str] = "resources",
     ) -> typer.Typer:
 
-        global STATIC_VERSION_FILE_PATH
-        STATIC_VERSION_FILE_PATH = version_file_path
+        global STATIC_VERSION_PACKAGE_PATH
+        STATIC_VERSION_PACKAGE_PATH = version_package_path
 
         if config_resolver_fn:
             config_resolver_fn()
