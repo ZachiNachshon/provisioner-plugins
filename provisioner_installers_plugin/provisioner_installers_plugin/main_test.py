e1984ef41d62f81297c1be04376f31e8ae901063
ZachiNachshon <zachi.nachshon@gmail.com>
2025-03-20 10:34:55 +0200

    [skip ci] Temporary commit

--- provisioner_installers_plugin/provisioner_installers_plugin/main_test.py
+++ provisioner_installers_plugin/provisioner_installers_plugin/main_test.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import unittest
+# import unittest
 
-from provisioner.main import root_menu
-from provisioner_shared.test_lib.test_cli_runner import TestCliRunner
+# from provisioner.main import root_menu
+# from provisioner_shared.test_lib.test_cli_runner import TestCliRunner
 
 
-# To run these directly from the terminal use:
-#  poetry run coverage run -m pytest -s plugins/provisioner_installers_plugin/provisioner_installers_plugin/main_test.py
-#
-class TestCLI(unittest.TestCase):
+# # To run these directly from the terminal use:
+# #  ./run_tests.py plugins/provisioner_installers_plugin/provisioner_installers_plugin/main_test.py
+# #
+# class TestCLI(unittest.TestCase):
 
-    def test_installers_plugin_appears_in_menu(self):
-        result = TestCliRunner.run(
-            root_menu,
-            [
-                "--help",
-            ],
-        )
-        self.assertIn("install", result)
-        self.assertIn("Install anything anywhere on any OS/Arch", result)
+#     def test_installers_plugin_appears_in_menu(self):
+#         result = TestCliRunner.run(
+#             root_menu,
+#             [
+#                 "--help",
+#             ],
+#         )
+#         self.assertIn("install", result)
+#         self.assertIn("Install anything anywhere on any OS/Arch", result)
--- provisioner_single_board_plugin/provisioner_single_board_plugin/main_test.py
+++ provisioner_single_board_plugin/provisioner_single_board_plugin/main_test.py
@@ -2,29 +2,21 @@
 
 import unittest
 
-import pytest
-
 from provisioner.main import root_menu
-from provisioner_shared.test_lib.docker.dockerized import dockerized
 from provisioner_shared.test_lib.test_cli_runner import TestCliRunner
-
-#
-# !! Run within a Docker container !!
-#
 
 
 # To run these directly from the terminal use:
-#  poetry run coverage run -m pytest -s plugins/provisioner_single_board_plugin/provisioner_single_board_plugin/main_test.py --e2e
+#  ./run_tests.py plugins/provisioner_single_board_plugin/provisioner_single_board_plugin/main_test.py
 #
-@pytest.mark.e2e
 class TestCLI(unittest.TestCase):
 
-    @dockerized(force_build=True)
-    def test_examples_plugin_appears_in_menu(self):
+    def test_single_board_plugin_appears_in_menu(self):
         result = TestCliRunner.run(
             root_menu,
             [
                 "--help",
             ],
         )
-        self.assertIn("single-board  Single boards management as simple as it gets", result)
+        self.assertIn("single-board", result)
+        self.assertIn("Single boards management as simple as it gets", result)
